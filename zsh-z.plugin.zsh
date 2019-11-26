2608b376dd8fb5ba118e137ab6acf38859476e11
Alexandros Kozak <git-agkozak@sneakemail.com>
2019-11-26 07:17:00 -0800

    Standardized $0 handling

--- zsh-z.plugin.zsh
+++ zsh-z.plugin.zsh
@@ -639,7 +639,9 @@ add-zsh-hook chpwd _zshz_chpwd
 
 # Standarized $0 handling
 # (See https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc)
-0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0=${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}
+0=${${(M)0:#/*}:-$PWD/$0}
+
 fpath=( ${0:A:h} $fpath )
 
 ############################################################
