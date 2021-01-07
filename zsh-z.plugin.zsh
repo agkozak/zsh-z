c02e2d29e4098d707029352f3c156e563a107af7
Alexandros Kozak <git-agkozak@sneakemail.com>
2021-01-07 08:55:13 -0800

    Remove trailing zeroes

--- zsh-z.plugin.zsh
+++ zsh-z.plugin.zsh
@@ -469,9 +469,10 @@ zshz() {
         # Sort results
         for x in ${(@on)output};do
           # Still using period as decimal separator for compatibility with fzf-z
-          LC_ALL=C printf '%-10.2f' $(( ${x%%[[:blank:]]*} / 10000. ))
-          print "/${x#*/}"
-          # print $x
+          LC_ALL=C _zshz_printv -f '%-10.2f' $(( ${x%%[[:blank:]]*} / 10000. ))
+          print "${REPLY/[[:punct:]]00/   }/${x#*/}"
+          REPLY=''
+          # print $x  # for rupa/z-like behavior
         done
         ;;
 
