d91c50e39d93022bc5cf9944e0cc4640ef7e366d
Alexandros Kozak <git-agkozak@sneakemail.com>
2021-01-11 21:11:49 -0800

    Use fold for help text

--- zsh-z.plugin.zsh
+++ zsh-z.plugin.zsh
@@ -90,7 +90,7 @@ With no ARGUMENT, list the directory history in ascending rank.
   -l    List all matches without going to them
   -r    Match by rank
   -t    Match by recent access
-  -x    Remove the current directory from the database" >&2
+  -x    Remove the current directory from the database" | fold -s >&2
 }
 
 # Load zsh/datetime module, if necessary
