69dfca878bb8df19d9771f9b53cc334402f6c13f
Alexandros Kozak <git-agkozak@sneakemail.com>
2021-02-08 21:26:37 -0800

    Use large integers for ranks, like rupa/z

--- zsh-z.plugin.zsh
+++ zsh-z.plugin.zsh
@@ -483,11 +483,7 @@ zshz() {
         # -l
         else
           for x in ${(@on)output}; do
-            # Still using period as decimal separator for compatibility with fzf-z
-            LC_ALL=C _zshz_printv -f '%-10.2f' $(( ${x%%[[:blank:]]*} / 10000. ))
-            printf '%-10s%s\n' "${REPLY/[[:punct:]]00/   }" "/${x#*/}"
-            REPLY=''
-            # print $x  # for rupa/z-like behavior
+            print $x
           done
         fi
         ;;
