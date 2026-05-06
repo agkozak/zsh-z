08e3e99267f0f59220195107bc0bc2c3ffec4812
Alexandros Kozak <git-agkozak@sneakemail.com>
2026-05-06 10:41:35 -0700

    Missing braces

--- zsh-z.plugin.zsh
+++ zsh-z.plugin.zsh
@@ -1025,7 +1025,7 @@ add-zsh-hook chpwd _zshz_chpwd
 
 # Standardized $0 handling
 # https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
-0="${${ZERO:-${0:#$ZSH_ARGZERO-}}:-${(%):-%N}}"
+0="${${ZERO:-${0:#${ZSH_ARGZERO-}}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
 (( ${fpath[(ie)${0:A:h}]} <= ${#fpath} )) || fpath=( "${0:A:h}" "${fpath[@]}" )
